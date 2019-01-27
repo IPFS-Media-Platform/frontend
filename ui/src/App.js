1531814b6cbb10269e8f76071d8dc654930366b7
Cael Rowley <caelrowley@gmail.com>
2019-01-27 22:00:44 +0000

    Add Auth0 Lock Screen
    The application needed access authenticaiton
    
    Implemeneted an Auth0 lock screen to allow the user to login to authenticate
    and access the protected pages.
    
    Related: 78-create-login-screen

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Public from './Components/Public';
+import Private from './Components/Private';
+import Login from './Auth/Login';
+import Logout from './Auth/Logout';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Header />
+    <main>
+      <Switch>
+        <Route exact path="/" component={Public} />
+        <Route path="/private" component={Private} />
+        <Route path="/login" component={Login} />
+        <Route path="/logout" component={Logout} />
+        }}/>
+      </Switch>
+    </main>
+  </div>
+)
 
-export default App;
+export default App
